6d4efb561dce8f8bd02d243efb7f74df9955a145
luisf18 <luisfelipefonte@gmai.com>
2023-09-27 08:03:09 -0300

    iniciando os trabalhos... .h .cpp e library.propeties

---
+++ src/DRV8833.h
@@ -0,0 +1,33 @@
+/* ====================================================
+ *  EasySensor Library
+ *    Author: luisf18 (github)
+ *    Ver.: 1.0.0
+ *    last_update: 04/12/2022
+ * ====================================================
+ */
+
+#ifndef DRV8833_H
+#define DRV8833_H
+#include <Arduino.h>
+
+
+class DRV8833
+{
+private:
+  /* data */
+public:
+  DRV8833(/* args */);
+  ~DRV8833();
+};
+
+DRV8833::DRV8833(/* args */)
+{
+}
+
+DRV8833::~DRV8833()
+{
+}
+
+
+
+#endif
